388fd9de10319bfaa719b8ccad9d87ac23465a6c
Jakub Andrysek <jakub.andrysek03@gmail.com>
2025-08-14 01:38:12 +0200

    feat: integrate BlocklyEditor into App component and update blockly-ui package configuration

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import BlocklyEditor from '@jacly/blockly-ui';
 
 function App() {
   return (
     <>
       <h1>Hi JacLy</h1>
+      <BlocklyEditor />
     </>
   );
 }
---
+++ packages/blockly-ui/src/index.ts
@@ -0,0 +1,2 @@
+export { default } from './BlocklyEditor';
+export { default as BlocklyEditor } from './BlocklyEditor';
