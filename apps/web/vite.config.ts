1de34976f1819028b966155a1b2a401192f923d5
Jakub Andrysek <jakub.andrysek03@gmail.com>
2025-11-01 19:06:08 +0100

    refactor: code structure for improved readability and maintainability

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { githubPagesSetup } from './src/vite/vite-plugin-github-pages-setup';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,5 +15,12 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
+    githubPagesSetup(),
+    nodePolyfills(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
