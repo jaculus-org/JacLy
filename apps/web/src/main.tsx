65901d2d309a0295feb31211b13aa1c2f04bd8f5
Jakub Andrysek <jakub.andrysek03@gmail.com>
2025-11-01 19:30:10 +0100

    feat: implement storage utility and theme provider for consistent state management

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,27 +2,30 @@ import './i18n/config';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
-
-// Import the generated route tree
+import SnackbarProviderCustom from './providers/snackbar-provider';
+import { ThemeProvider } from './providers/theme-provider';
 import { routeTree } from './routeTree.gen';
 
-// Create a new router instance
 const router = createRouter({ routeTree });
 
-// Register the router instance for type safety
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router;
   }
 }
 
-// Render the app
 const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <SnackbarProviderCustom>
+        <ThemeProvider>
+          {/* <JacProvider> */}
+          <RouterProvider router={router} />
+          {/* </JacProvider> */}
+        </ThemeProvider>
+      </SnackbarProviderCustom>
     </StrictMode>
   );
 }
